b52f53fc5a7ebd94089d7814c74ea1d65e2b4357
Nick Davis <nicholas.davis@tufts.edu>
2014-02-23 20:39:51 -0500

    first few files for app

---
+++ js/app.js
@@ -0,0 +1,8 @@
+define(['jquery', 'underscore', 'backbone', 'router'], function($, _, Backbone, Router) {
+  var initialize = funtion() {
+    Router.initialize();
+  }
+  return {
+    initialize: initialize;
+  };
+});
